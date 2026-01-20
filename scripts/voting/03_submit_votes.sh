5769ab7a6e5dc6f30bd2d45b9eb17dc4328c0b70
HephyPool <82511049+hephy-io@users.noreply.github.com>
2026-01-20 19:19:52 +0000

    Create 03_submit_votes.sh

---
+++ scripts/voting/03_submit_votes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DATE_STAMP=$(date +%d%m%y)
+TX_BODY="${DATE_STAMP}-tx.body"
+
+# Build witness arguments based on what was transferred from COLD environment
+WITNESS_ARGS="--witness-file payment.witness"
+[ -f drep.witness ] && WITNESS_ARGS+=" --witness-file drep.witness"
+[ -f spo.witness ] && WITNESS_ARGS+=" --witness-file spo.witness"
+
+# Assemble Witness Signatures
+cardano-cli conway transaction assemble --tx-body-file "$TX_BODY" $WITNESS_ARGS --out-file "${DATE_STAMP}-tx.signed"
+# Submit transaction and copy output to .txt file for future reference
+cardano-cli conway transaction submit --tx-file "${DATE_STAMP}-tx.signed" --testnet-magic 4 2>&1 | tee tx_hash.txt
+
+# Archive everything
+mkdir -p archive/"$DATE_STAMP"
+mv *.vote *.witness *.body *.signed *.txt archive/"$DATE_STAMP"/
